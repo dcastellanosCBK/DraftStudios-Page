d2b63fe52a3525e35259f6138a3a8e4f270342e3
davidcastel <davidccbp20@gmail.com>
2020-01-05 13:46:18 -0500

    Added Comments on the clRequestForm function

--- js/main.js
+++ js/main.js
@@ -325,70 +325,22 @@
 
 	/* Contact Form
     * ------------------------------------------------------ */
-	var clContactForm = function() {
-
-		/* local validation */
-		$('#contactForm').validate({
-
-			/* submit via ajax */
-			submitHandler: function(form) {
-
-				var sLoader = $('.submit-loader');
-
-				$.ajax({
-
-					type: "POST",
-					url: "inc/sendEmail.php",
-					data: $(form).serialize(),
-					beforeSend: function() { 
-
-						sLoader.slideDown("slow");
-
-					},
-					success: function(msg) {
-
-						// Message was sent
-						if (msg == 'OK') {
-							sLoader.slideUp("slow"); 
-							$('.message-warning').fadeOut();
-							$('#contactForm').fadeOut();
-							$('.message-success').fadeIn();
-						}
-						// There was an error
-						else {
-							sLoader.slideUp("slow"); 
-							$('.message-warning').html(msg);
-							$('.message-warning').slideDown("slow");
-						}
-
-					},
-					error: function() {
-
-						sLoader.slideUp("slow"); 
-						$('.message-warning').html("Something went wrong. Please try again.");
-						$('.message-warning').slideDown("slow");
-
-					}
-
-				});
-			}
-
-		});
-	};
-
-
 	var clRequestForm = function() {
 		$(document).ready(function () {
+			// When you click the 'x' 
 			$(".mode-close").click(function () {
+				// The Form slides up
 				$(".disForm").slideUp(1000);
 				$(".s-contact").css("background-position", "center top");
 			});
+			// When you click the 'Request a Form Button
 			$(".formBtn").click(function () {
+				// The Form will either toggle between sliding up or down depending on the state that is in
 				$(".disForm").slideToggle(1500);
 			});
 		});
 	};
-
+	
 
 	/* Animate On Scroll
     * ------------------------------------------------------ */
@@ -474,7 +426,6 @@
 		clSmoothScroll();
 		clPlaceholder();
 		clAlertBoxes();
-		clContactForm();
 		clRequestForm();
 		clAOS();
 		clAjaxChimp();
